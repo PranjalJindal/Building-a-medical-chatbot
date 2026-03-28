66a62da08259da9deddebe66a416572372cc71b6
Pranjal Jindal <pranjaljindal06@gmail.com>
2026-03-28 16:26:25 +0530

    Requirement and text file updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Pranjal Jindal",
+    author_email="pranjaljindal06@gmail.com",
+    packages=find_packages(),
+    include_requires=[]
+)
